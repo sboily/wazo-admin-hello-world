a29a5fdee5f767b0f8eb050c7d941f292fea887a
Sylvain Boily <sylvainboiydroid@gmail.com>
2017-02-23 09:37:23 -0500

    use login required view

--- hello_world/view.py
+++ hello_world/view.py
@@ -7,9 +7,9 @@ from __future__ import unicode_literals
 from flask import render_template
 
 from flask_menu.classy import classy_menu_item
-from flask_classful import FlaskView
+from wazo_admin_ui.helpers.classful import LoginRequiredView
 
-class HelloWorldView(FlaskView):
+class HelloWorldView(LoginRequiredView):
 
     @classy_menu_item('.hello', 'Hello World', order=1, icon="heart")
     def index(self):
